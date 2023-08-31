4e2f63bd96c35a9c665abbf3b717a340cfe85bdc
muchen <muchen@lsm.com>
2023-08-31 12:32:18 +0800

    somin first commit

--- offer/src/_01_58_II_左旋转字符串.java
+++ offer/src/_01_58_II_左旋转字符串.java
@@ -3,11 +3,10 @@ public class _01_58_II_×óÐý×ª×Ö·û´® {
     public static void main(String[] args) {
         int n = 2;
         String s = "abcdefg";
-//        System.out.println(s.substring(n) + s.substring(0,n));
+        System.out.println(s.substring(n) + s.substring(0,n));
 
         StringBuilder tail = new StringBuilder();
         StringBuilder head = new StringBuilder();
-
         for (int i = 0; i < s.length(); i++) {
             if (i < n) tail.append(s.charAt(i));
             else head.append(s.charAt(i));
